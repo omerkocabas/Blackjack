f44d1267b866799f7527c2507284206a023c0fc0
Omer Kocabas <omerkamilkocabas@gmail.com>
2021-02-17 13:00:36 +0300

    Bug fixes

--- Blackjack.cpp
+++ Blackjack.cpp
@@ -1,23 +1,25 @@
-﻿// ConsoleApplication2.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+// ConsoleApplication2.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
 //
 
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <cstdlib>
+#include <string>
 using namespace std;
 
 
 class Card {
-    char name;
+    string name;
     int value;
 public:
-    Card(char name, int value) :name(name), value(value) {};
+    Card(string name, int value) :name(name), value(value) {};
     
-    char getMyName() {
+    string getCardName() {
         return name;
     }
 
-    int getMyValue() {
+    int getCardValue() {
         return value;
     }
 
@@ -36,68 +38,86 @@ public:
         srand(time(0));
         cout << "Welcome to Omer's Casino!" << endl;
         cout << "For now we only have Blackjack without split. I hope you enjoy the time. Remember casino always wins :P" << endl;
-        for (int i = 0; i < 2; i++) {
-            setCard(Player);
-            setCard(Casino);
-        }
+        initializeCards();
     };
-    void setCard(vector<Card>& myVector) {
-        int myVal=0;
-        char myName;
+    
+   
+    void setCard(vector<Card>& card_vector) {
+        int cardValue=0;
+        string cardName;
 
         int x = rand()%13+2;
-        if (x <= 10) {
-            myVal = x;
-            myName = (char) x;
-            myVector.push_back(Card(myName, myVal));
+        if (x < 10) {
+            cardValue = x;
+            cardName = (char) (x+48);
+            card_vector.push_back(Card(cardName, cardValue));
         }
         else {
             switch (x) {
+        	case 10:
+        		cardValue = 10;
+                cardName = "10";
+                card_vector.push_back(Card(cardName, cardValue));
+                break;
             case 11:
-                myVal = 10;
-                myName = 'J';
-                myVector.push_back(Card(myName, myVal));
+                cardValue = 10;
+                cardName = 'J';
+                card_vector.push_back(Card(cardName, cardValue));
                 break;
             case 12:
-                myVal = 10;
-                myName = 'Q';
-                myVector.push_back(Card(myName, myVal));
+                cardValue = 10;
+                cardName = 'Q';
+                card_vector.push_back(Card(cardName, cardValue));
                 break;
             case 13:
-                myVal = 10;
-                myName = 'K';
-                myVector.push_back(Card(myName, myVal));
+                cardValue = 10;
+                cardName = 'K';
+                card_vector.push_back(Card(cardName, cardValue));
+                break;
             case 14:
-                myVal = 11;
-                myName = 'A';
-                myVector.push_back(Card(myName, myVal));
+                cardValue = 11;
+                cardName = 'A';
+                card_vector.push_back(Card(cardName, cardValue));
             }
+        }
+    }
+    
+     void initializeCards(){
+    	for (int i = 0; i < 2; i++) {
+            setCard(Player);
+            setCard(Casino);
         }
     }
 
     void Draw(vector<Card> myVector) {
-            for (int i = 0; i < 5; i++) {
+    	int card_width = 9;
+    	int card_length= 5;
+            for (int i = 0; i < card_length; i++) {
 
                 
-                for (int j = 0; j < 8 * myVector.size()+2 ; j++) {
+                for (int j = 0; j < card_width * myVector.size() ; j++) {
 
-                    if ((i == 0||i==4)&& (j % 9 != 0)&& (j % 9 != 7)&& (j % 9 != 8)) {
+                    if ((i == 0||i==(card_length-1))&& (j % card_width != 0)&& (j % card_width != card_width-2)&& (j % card_width != (card_width-1))) {
                             cout << "-";
                     }
 
 
 
-                    else if (j % 9 == 0 || j % 9 == 7) {
+                    else if (j % card_width == 0 || j % card_width == (card_width-2)) {
                         cout << "|";
                     }
                     
-                    else if (i == 2 && j%9 == 4) {
-                        int temp = j / 9;
-                        if (myVector[temp].getMyValue() >= 10) {
+                    else if (i == (card_length/2) && (j%card_width) == (card_width/2)) {
+                        int card_number = j / card_width;
+                        //indicates which card is being drawed.
+                        if (myVector[card_number].getCardName() == "10") {
                             cout<<"\b";
-                        }
-                        cout << myVector[temp].getMyValue();
-                        //cout << "J";
+                            cout<<"10";
+                            //because 10 has 2 digits, one backspace is required.
+                        }else{
+                        	cout << myVector[card_number].getCardName();
+						}
+
                     }
 
                     else {
@@ -121,25 +141,68 @@ public:
 
 
 
-    void gameEnd(){
+    void gameRestart(){
 
         Player.clear();
         Casino.clear();
 
-        for (int i = 0; i < 2; i++) {
-            setCard(Player);
-            setCard(Casino);
-        }
+        initializeCards();
         
     }
     
     int getSum(vector<Card> myVector) {
-        int sum = 0;
-        for (int i = 0; i < myVector.size(); i++) {
-            sum += myVector[i].getMyValue();
+    	int ace_counter=0;
+    	int total_value=0;
+    	for (int i = 0; i < myVector.size(); i++) {
+        	if(myVector[i].getCardName()=="A"){
+        		ace_counter +=1;	
+			}
+			
+			total_value += myVector[i].getCardValue();
         }
-        return sum;
+        
+        for(int i=0;i<ace_counter;i++){
+        	if(total_value>21){
+        		total_value -= 10;
+			}
+			
+		}
+    	return total_value;
     }
+    
+    bool askDrawCard(){
+    	cout << "Do you want one more card?(If yes press y, if no press n)" << endl;
+        while(true){
+		
+    		char player_response;
+        	cin >> player_response;
+        	if (player_response== 'y') {
+            	return true;
+        	}else if (player_response == 'n') {
+            	return false;
+        	}else{
+        		cout<<"Oops, wrong button!"<<endl;
+			}
+		}
+    }
+    
+    
+    bool askGameEnd(){
+        cout << "Do you want to play one more time?(If yes press y, if no press n)" << endl;
+        while(true){
+		
+    		char player_response;
+        	cin >> player_response;
+        	if (player_response== 'y') {
+            	return false;
+        	}else if (player_response == 'n') {
+            	cout << "I guess you are out of money. See you!" << endl;
+            	return true;
+        	}else{
+        		cout<<"Oops, wrong button!"<<endl;
+			}
+		}
+	}
 
     void GameRun() {
         while (true) {
@@ -149,61 +212,43 @@ public:
 
 
             if (getSum(Player) > 21) {
-                cout << "You lost the game! Better luck next time." << endl;
-                cout << "Do you want to play one more time?(If yes press y, if no press n)" << endl;
-                char q;
-                cin >> q;
-                if (q== 'y') {
-                    gameEnd();
-                }
-                else if (q == 'n') {
-                    cout << "I guess you are out of money. See you!" << endl;
+            	cout << "You lost the game!" << endl;
+                bool game_end = askGameEnd();
+                if (!game_end) {
+                    gameRestart();
+                }else{
                     break;
                 }
-            }
-            else {
+            }else {
 
-                cout << "Do you want one more card?(If yes press y, if no press n)" << endl;
-                char u;
-                cin >> u;
-                if (u == 'y') {
+                bool response = askDrawCard();
+                
+                if (response) {
                     setCard(Player);
-                }
-                else if (u == 'n') {
-                    while (true) {
-                        if (getSum(Casino) < 17 && getSum(Casino) < getSum(Player)) {
+                }else {
+                    while (getSum(Casino) < 17) {
                             setCard(Casino);
                             drawcasino();
-                        }
-                        else {
-                            
-                            if (getSum(Casino) > getSum(Player)&&getSum(Casino)<=21) {
-                                cout << "You lost!" << endl;
-                            }
-                            else if(getSum(Casino)==getSum(Player)){
-                                cout << "It's a tie!" << endl;
-                            }
-                            else {
-                                cout << "You won!" << endl;
-                            }
-
-                            cout << "Do you want to play one more time?(If yes press y, if no press n)" << endl;
-                            char q;
-                            cin >> q;
-                            if (q == 'y') {
-                                gameEnd();
-                            }
-                            else if (q == 'n') {
-                                cout << "I guess you are out of money. See you!" << endl;
-                                break;
-                            }
-                            break;
-                        }
                     }
+                            
+                    if (getSum(Casino) > getSum(Player)&&getSum(Casino)<=21) {
+                        cout << "You lost!" << endl;
+                    }else if(getSum(Casino)==getSum(Player)){
+                        cout << "It's a tie!" << endl;
+                    }else {
+                        cout << "You won!" << endl;
+                    }
+                    
+                    gameRestart();
+
+                    bool game_end = askGameEnd();
+                    if(game_end){
+                    	break;
+					}
+                    
                 }
-                else {
-                    cout << "You pressed a wrong button!" << endl;
-                }
+                
+                
             }
 
             
@@ -217,7 +262,7 @@ int main()
 {
     Game a;
     a.GameRun();
-  
+    return 0;
 }
 
 // Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
